4fd570ad45ff3eacf5248579e2818585aab663e2
phanyoung <phanyoung@pypot.com>
2014-12-04 14:36:27 +0800

    add comment

--- ch_04/addr_sim.py
+++ ch_04/addr_sim.py
@@ -8,6 +8,14 @@ EARTH_R = 6371.004
 AK_CODE = 'hGYEy6hgmrw1LPoVt9zKfH4x'
 
 def GetGeoCoord(addr, city):
+    ''' 利用百度的web api获取指定地址的经纬度
+    [Parameters]
+        addr<str>: 地址字符串
+        city<str>: 城市
+    [Return]
+        tuple<float, float>: 经度,纬度
+                  如果异常，则返回None 
+    '''
     requstUrl = "http://api.map.baidu.com/geocoder/v2/?address=%s&city=%s&output=json&ak=%s" % (addr, city, AK_CODE)
     print requstUrl
     socket = None
@@ -28,6 +36,12 @@ def GetGeoCoord(addr, city):
 
 
 def CalcGeoCoordDist(coord1, coord2):
+    ''' 计算两个给定坐标的空间距离
+    [Parameters]
+        coord1<tuple2>, coord2<tuple2>: 比较的两个从标，第一项为经度，第二项为纬度
+    [Return]
+       <float>: 两个输入坐标的空间距离（单位km)
+    '''
     radLng1 = coord1[0] / 180.0 * numpy.pi
     radLng2 = coord2[0] / 180.0 * numpy.pi
     radLat1 = coord1[1] / 180.0 * numpy.pi
@@ -37,11 +51,18 @@ def CalcGeoCoordDist(coord1, coord2):
 
 
 
-def AddrDist(addr1, addr2, city):
-    coord1 = GetGeoCoord(addr1, city)
+def AddrDist(addr1, city1, addr2, city2):
+    ''' 计算两个地址的空间距离
+    [Parameters]
+        addr1<str>, addr2<str>: 比较的两个地址字符串
+        city1<str>, city2<str>: 两个给定地址所在城市 
+    [Return]
+       <float>: 两个输入地址的空间距离（单位km)
+    '''
+    coord1 = GetGeoCoord(addr1, city1)
     if not coord1:
         return None
-    coord2 = GetGeoCoord(addr2, city)
+    coord2 = GetGeoCoord(addr2, city2)
     if not coord2:
         return None
     return CalcGeoCoordDist(coord1, coord2)
